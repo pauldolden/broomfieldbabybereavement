55db260fc0526f451555a316506c5747f6c1535b
Paul Dolden <paul@dolden.co.uk>
2022-09-04 09:41:56 +0100

    WIP dynamic header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    image(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image(), sitemap(), svelte()]
 });
---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { inview, Options } from "svelte-inview";
+  import StaticHeader from "@components/StaticHeader.svelte";
+  import ScrollingHeader from "@components/ScrollingHeader.svelte";
+
+  let isInvIew: boolean;
+
+  function handleChange({ detail }: CustomEvent<ObserverEventDetails>) {
+    isInvIew = detail.inView;
+  }
+
+  const options: Options = {};
+</script>
+
+<section use:inview={options} on:change={handleChange} id="header">
+  {#if isInvIew}
+    <StaticHeader />
+  {:else}
+    <ScrollingHeader />
+  {/if}
+</section>
