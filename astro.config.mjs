99e8a3305c7b4406c4bf1f9c39bf16c02032b207
Paul Dolden <paul@dolden.co.uk>
2022-09-04 07:52:51 +0100

    set up prettier project config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
@@ -6,5 +6,11 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), sitemap()]
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    image(),
+    sitemap(),
+  ],
 });
